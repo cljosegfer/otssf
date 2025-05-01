bd0a220ae5dfaa158875dda26e4b9577554d24a6
cljosegfer <macaverde604@gmail.com>
2025-05-01 03:17:17 -0300

    more into arrows rebalance

--- data/scripts/spells/conjuring/conjure_onyx_arrow.lua
+++ data/scripts/spells/conjuring/conjure_onyx_arrow.lua
@@ -1,20 +1,20 @@
 local spell = Spell("instant")
 
 function spell.onCastSpell(creature, variant)
-	return creature:conjureItem(0, 763, 100, CONST_ME_MAGIC_BLUE) -- change arrow id
+	return creature:conjureItem(0, 14251, 100, CONST_ME_MAGIC_BLUE) -- change arrow id
 end
 
-spell:name("Conjure Flaming Arrow") -- change arrow name
-spell:words("exevo con flam") -- change word
+spell:name("Conjure Tarsal Arrow") -- change arrow name
+spell:words("exevo con gran hur") -- change word
 spell:group("support")
 -- spell:vocation("paladin;true", "royal paladin;true")
 spell:castSound(SOUND_EFFECT_TYPE_SPELL_CONJURE_ARROW)
 -- spell:id(49) -- change spell id
 spell:cooldown(2 * 1000)
 spell:groupCooldown(2 * 1000)
-spell:level(25)
-spell:mana(290)
-spell:soul(30)
+spell:level(30)
+spell:mana(390)
+spell:soul(40)
 spell:isSelfTarget(true)
 spell:isAggressive(false)
 spell:needLearn(false)
