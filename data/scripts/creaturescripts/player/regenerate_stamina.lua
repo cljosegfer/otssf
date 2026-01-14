069d9cbfe93e1b882da1d63c4063db6199f81144
remote <macaverde604@gmail.com>
2026-01-14 00:31:45 -0300

    remove stamine

--- data/scripts/creaturescripts/player/regenerate_stamina.lua
+++ data/scripts/creaturescripts/player/regenerate_stamina.lua
@@ -5,28 +5,31 @@ function regenerateStamina.onLogin(player)
 		return true
 	end
 
-	local lastLogout = player:getLastLogout()
-	local offlineTime = lastLogout ~= 0 and math.min(os.time() - lastLogout, 86400 * 21) or 0
+	player:setStamina(2520)
+    return true
 
-	offlineTime = offlineTime - 600
+	-- local lastLogout = player:getLastLogout()
+	-- local offlineTime = lastLogout ~= 0 and math.min(os.time() - lastLogout, 86400 * 21) or 0
 
-	if offlineTime < 180 then
-		return true
-	end
+	-- offlineTime = offlineTime - 600
 
-	local staminaMinutes = player:getStamina()
-	local maxNormalStaminaRegen = 2340 - math.min(2340, staminaMinutes)
-	local regainStaminaMinutes = offlineTime / 180
+	-- if offlineTime < 180 then
+	-- 	return true
+	-- end
 
-	if regainStaminaMinutes > maxNormalStaminaRegen then
-		local happyHourStaminaRegen = (offlineTime - (maxNormalStaminaRegen * 180)) / 360
-		staminaMinutes = math.min(2520, math.max(2340, staminaMinutes) + happyHourStaminaRegen)
-	else
-		staminaMinutes = staminaMinutes + regainStaminaMinutes
-	end
+	-- local staminaMinutes = player:getStamina()
+	-- local maxNormalStaminaRegen = 2340 - math.min(2340, staminaMinutes)
+	-- local regainStaminaMinutes = offlineTime / 180
 
-	player:setStamina(staminaMinutes)
-	return true
+	-- if regainStaminaMinutes > maxNormalStaminaRegen then
+	-- 	local happyHourStaminaRegen = (offlineTime - (maxNormalStaminaRegen * 180)) / 360
+	-- 	staminaMinutes = math.min(2520, math.max(2340, staminaMinutes) + happyHourStaminaRegen)
+	-- else
+	-- 	staminaMinutes = staminaMinutes + regainStaminaMinutes
+	-- end
+
+	-- player:setStamina(staminaMinutes)
+	-- return true
 end
 
 regenerateStamina:register()
